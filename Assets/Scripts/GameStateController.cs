08004b2c43dde1d07cdda20e553f6d86913e23f1
ctkou <adamkouct@gmail.com>
2017-01-22 01:29:23 -0800

    add target

--- Assets/Scripts/GameStateController.cs
+++ Assets/Scripts/GameStateController.cs
@@ -5,23 +5,57 @@ using UnityEngine.UI;
 
 public class GameStateController : MonoBehaviour {
     private int score;
-    public Text displayText;
-    public Text gameOverMessage;
+    public Text gameOverMessage, gameWinMessage;
+    
+    public Text displayText, stateText;
 
+	public int hitCounts;
 
 	// Use this for initialization
 	void Start () {
+		hitCounts = 0;
 		score = 0;
 		addScore(0);
+		updateText();
+		showGameOverMessage(false);
+		showGameWinMessage(false);
 	}
-
-    public void addScore(int n) {
-        Debug.Log("addScore");
-        score += n;
-        displayText.text = "Score: " + score.ToString();
-    }
 
     public void showGameOverMessage(bool isShown) {
         gameOverMessage.gameObject.SetActive(isShown);
     }
+
+	public void showGameWinMessage(bool isShown) {
+		gameWinMessage.gameObject.SetActive(isShown);
+	}
+
+    public void addScore(int n) {
+      	score += n;
+      	displayText.text = "Score: " + score.ToString();
+  	}
+
+	public void onSnakeEnter(TriggerController tc) {
+		hitCounts++;
+		update();
+	}
+
+	public void onSnakeExit(TriggerController tc) {
+		hitCounts--;
+		update();
+	}
+	private void update() {
+		if (remainingCount() == 0) {
+			showGameWinMessage(true);
+		} else {
+			updateText();
+		}
+	}
+
+	private int remainingCount() {
+		return 2 - hitCounts;
+	}
+
+	private void updateText() {
+		stateText.text = "Remaining: " + remainingCount().ToString();
+	}
 }
